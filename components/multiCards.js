512031f69fec44eaf7fb1d9fe045dfc11d0c569b
Anton <antonkoller@outlook.com>
2023-03-29 14:39:01 +0200

    fetch testing

--- components/multiCards.js
+++ components/multiCards.js
@@ -2,11 +2,20 @@ import Card from "./card";
 import styles from "./multiCards.module.scss";
 
 export default function MultiCards() {
+  const fetchRandom = () => {
+    fetch("http://localhost:3000/api/fetchtest")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
   return (
     <>
       <h3 style={{ color: "#454545", marginLeft: "80px" }}>
         Showing results for ´INPUT´:
       </h3>
+      <button onClick={fetchRandom}>Fetch</button>
       <section className={styles.cardGrid}>
         <Card />
         <Card />
