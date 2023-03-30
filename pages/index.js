c2f225bb8059cdad95451cc9eb19a70ff9ec7b5d
Agata Janiszewska <agatajaniszewska@Agatas-MacBook-Air.local>
2023-03-30 09:28:54 +0200

    Added search filter and functionality

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import SearchFilter from "@/components/searchfilter";
 import Navbar from "@/components/navbar";
 import Head from "next/head";
 
@@ -11,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
+      <SearchFilter></SearchFilter>
     </>
   );
 }
